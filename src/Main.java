c71a3223c8aa3c5c5fc6a1e96667a8e0174997d2
omar <itsomar278@gmail.com>
2023-07-25 09:48:59 +0300

    Singleton Design Pattern DONE

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args)
+    {
         System.out.println("Hello world!");
+        singleton s1 = singleton.getInstance();
+        s1.print();
+        singleton s2 = singleton.getInstance();
+        s2.print();
+
     }
 }
